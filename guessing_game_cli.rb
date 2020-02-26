36766414a4c04958fd2dd994756f9869fe6f9773
Tyler Lai <lai.t.tyler@gmail.com>
2020-02-26 19:11:37 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,7 +4,7 @@ def run_guessing_game
   puts "Guess a number between 1 and 6."
   user_guess = gets.chomp
   
-  if user_guess == random_number.to_i
+  if user_guess.to_i == random_number
     puts "You guessed the correct number!"
   elsif user_guess == "exit" 
     puts "Goodbye!"
